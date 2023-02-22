9ec5fd1e54e3b74b8b9f90740e55cac31da07f03
Tusharbecoding <tusharchopra02122002@gmail.com>
2023-02-22 22:17:35 +0530

    fixed standard form

--- standard_form.m
+++ standard_form.m
@@ -12,11 +12,10 @@ s = eye(size(A,1));
 ind = find(IneqSign > 0);
 s(ind, :) = -s(ind,:); 
 
-objFns = 
-Constraint = array2table(Mat);array2table(C);
-objFns.Properties.VariableNames(1:size(C,2)) = {'x_1','x_2'};
+objFns = array2table(C)
+objFns.Properties.VariableNames(1:2) = {'x_1','x_2'};
 Mat = [A s b];
-
+Constraint = array2table(Mat);
 Constraint.Properties.VariableNames(1:size(Mat,2)) = {'x_1','x_2','s_1','s_2','s_3','Sol'};
 
 objFns
